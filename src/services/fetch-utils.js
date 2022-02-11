1b478a4260c8ddae74a11ba571db74110a0ee530
Benatovich <sambenatovich@gmail.com>
2022-02-11 14:30:33 -0500

    file tree is set up and fetch functions are written in fetch-utils

---
+++ src/services/fetch-utils.js
@@ -0,0 +1,49 @@
+import { client, checkError } from './client';
+
+export function getUser() {
+  return client.auth.session();
+}
+
+export async function signUp(email, password) {
+  const response = await client.auth.signUp({ email, password });
+
+  return response.user;
+}
+
+export async function signIn(email, password) {
+  const response = await client.auth.signIn({ email, password });
+
+  return response.user;
+}
+
+export async function logout() {
+  await client.auth.signOut();
+
+  return window.location.href = '../';
+}
+
+export async function createGame(game){
+  const response = await client
+    .from('games')
+    .insert([game]);
+
+  return checkError(response);
+}
+
+export async function getGames() {
+  const response = await client
+    .from('games')
+    .select();
+
+  return checkError(response);
+}
+
+export async function getGameById(id) {
+  const response = await client
+    .from('games')
+    .select()
+    .match({ id })
+    .single();
+
+  return checkError(response);
+}
